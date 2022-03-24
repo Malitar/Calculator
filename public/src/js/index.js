32493de93f08e4b8a9f25a26348d4c337bcf888c
Alexander Symanzik <Alexander.Symanzik@provitex.de>
2022-03-24 09:37:31 +0100

    Include basic Math operations

---
+++ public/src/js/index.js
@@ -0,0 +1,24 @@
+/* ************************
+ * MATH FUNCTIONS
+************************ */
+
+function add(num1, num2) {
+    return num1+num2;
+}
+
+function sub(num1, num2){
+    return num1-num2;
+}
+
+function multiply(num1, num2) {
+    return num1*num2;
+}
+
+function divide(num1, num2) {
+    return num1/num2;
+}
+
+console.log(add(1,2));
+console.log(sub(1,2));
+console.log(multiply(12, 2));
+console.log(divide(4,2));
