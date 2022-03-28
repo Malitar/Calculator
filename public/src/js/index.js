f18d2f4cd63a50ddbc5beca00e64d56d01804f86
Alexander Symanzik <Alexander.Symanzik@provitex.de>
2022-03-28 10:32:05 +0200

    Function to display and clear userinput

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -70,17 +70,65 @@ const operator = function (num1, num2, operator) {
     }
 };
 
-const adjustOperator = (operator) => {
+function display(calcInput){
 
-    const processedOperator = operator.replace(/![+\-\*\/]/g, " ");
-    console.table(processedOperator);
-    //processedreplace
-
-    return processedOperator;
+    const input = calcInput.target.getAttribute("data-key");
+    switch(input) {
+        case 'reset': 
+            globalInput = ``;
+            clear();
+            break;
+        default: 
+            globalInput += input;
+            console.table(`${globalInput}`);
+            update(globalInput);
+    }
 }
 
+
+function update(userInput) {
+    /**
+     * [] get user input 
+     * [] get divs to manipulate
+     * [] update userInput => string to array
+     * [] string to array => 3 variablen
+     * [] display input
+     */
+    
+    console.log(userInput);
+    const currentInput = document.querySelector('.display');
+    let inputchild = document.createElement('span');
+    if(currentInput.hasChildNodes()){
+        clear();
+    }
+ 
+    inputchild.textContent = userInput;
+    console.log(inputchild);
+    currentInput.appendChild(inputchild);
+    
+}
+
+function clear(){
+    const clear = document.querySelector('.display');
+    const child = clear.lastElementChild;
+    if(clear.hasChildNodes()){
+        clear.removeChild(child);
+    }
+    
+}
+
+function reset() {
+    const reset = document.querySelector(`.display`);
+
+}
+
+/* 
 console.log(operator(1, 2, '+'));
 console.log(operator(1, 2, '-'));
 console.log(operator(4, 2, '*'));
 console.log(operator(4, 2, '/'));
 console.log(operator(4, 2, '12'));
+ */
+const calculatorBtn = document.querySelectorAll('button');
+let globalInput = ``;
+calculatorBtn.forEach(btn => btn.addEventListener('click', display));
